9ba95c6cc37734799251d38d3dfb13e1f3819a71
Sevra Genc <sevra.genc@futurance.com>
2024-01-23 14:21:33 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("org.jetbrains.kotlin.plugin.serialization") version "1.7.21"
+
 }
 
 android {
@@ -38,7 +40,7 @@ android {
     }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "META-INF/*.kotlin_module"
         }
     }
     buildFeatures {
