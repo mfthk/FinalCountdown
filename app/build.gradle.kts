f51ca0d09ca798a33c02c7795971de989551eb13
Sevra Genc <sevra.genc@futurance.com>
2024-01-23 10:57:10 +0300

    minSdk decreased 28 to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mfatihk.countdown"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
